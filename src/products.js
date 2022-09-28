6b8507660db615eac65bc35dee527f6199373f21
venkataleela02 <a.leelateja@gmail.com>
2022-09-28 15:06:20 +0530

    Commiting the final changes

---
+++ src/products.js
@@ -0,0 +1,82 @@
+const products = [
+  {
+    _id: "1",
+    name: "Staglin Family Vineyard",
+    place: "France Sauternes",
+    image: "/images/a1.jpg",
+    rating:
+      "RP 95",
+    vintage: "1950s",
+    qty: 4,
+    volume: "Standard (750 ml)",
+    cost: "$56.95",
+    price: "$59.95"
+  },
+  {
+    _id: "2",
+    name: "Bibi Graetz Testamatta",
+    place: "Chianti Blends and others",
+    image: "/images/a2.jpg",
+    rating:
+      "RP 95",
+    vintage: "2015",
+    qty: 32,
+    volume: "Magnum (1500 ml)",
+    cost: "$71.95",
+    price: "$72.95"
+  },
+  {
+    _id: "3",
+    name: "Cayuse Syrah en Chamber",
+    place: "Syrah",
+    image: "/images/a3.jpg",
+    rating:
+      "RP 95",
+    vintage: "2012",
+    qty: 12,
+    volume: "Standard (750 ml)",
+    cost: "$139.95",
+    price: "$150.95"
+  },
+  {
+    _id: "4",
+    name: "Staglin Family Vineyard",
+    place: "United States Clifornia Blends",
+    image: "/images/a4.jpg",
+    rating:
+      "RP 95",
+    vintage: "2010",
+    qty: 4,
+    volume: "Odd Ball (5000 ml)",
+    cost: "$56.95",
+    price: "$59.95"
+  },
+  {
+    _id: "5",
+    name: "K Vintners Syrah Cattle King",
+    place: "France Sauternes",
+    image: "/images/a5.jpg",
+    rating:
+      "RP 95",
+    vintage: "2015",
+    qty: 25,
+    volume: "Magnum (1500 ml)",
+    cost: "$79.95",
+    price: "$2.95"
+  },
+  {
+    _id: "6",
+    name: "L'Ecole No. 41 Ferguson",
+    place: "United States Clifornia Blends",
+    image: "/images/a6.jpg",
+    rating:
+      "RP 95",
+    vintage: "2004",
+    qty: 4,
+    volume: "Odd Ball (5000 ml)",
+    cost: "$56.95",
+    price: "$59.95"
+  }
+];
+
+export default products;
